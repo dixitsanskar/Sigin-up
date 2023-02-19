376e5b44252ffd5c58610afd1cb703d823c1d1a9
dixitsanskar <sanskar.sanjay@gmail.com>
2023-02-19 19:22:27 +0530

    initial commit

--- app/src/main/java/com/example/signuptest/LoginActivity.kt
+++ app/src/main/java/com/example/signuptest/LoginActivity.kt
@@ -1,11 +1,55 @@
 package com.example.signuptest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var redirectSignup: TextView
+    lateinit var etEmail: EditText
+    private lateinit var etPass: EditText
+    lateinit var LoginButton: Button
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        redirectSignup = findViewById(R.id.textViewsignup)
+        etEmail = findViewById(R.id.email)
+        etPass = findViewById(R.id.pass)
+        LoginButton = findViewById(R.id.loginbutton)
+
+        auth = FirebaseAuth.getInstance()
+
+        LoginButton.setOnClickListener {
+            login()
+        }
+        redirectSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun login() {
+
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this){
+            if (it.isSuccessful)
+            {
+                Toast.makeText(this, "Successfully Logged IN", Toast.LENGTH_SHORT).show()
+            }
+            else
+                Toast.makeText(this, "Log In failed", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
--- app/src/main/java/com/example/signuptest/SignupActivity.kt
+++ app/src/main/java/com/example/signuptest/SignupActivity.kt
@@ -1,11 +1,59 @@
 package com.example.signuptest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthWebException
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
+    lateinit var etEmail: EditText
+    lateinit var userName : EditText
+    private lateinit var etPass : EditText
+    private lateinit var btnSignUp : Button
+    private lateinit var auth: FirebaseAuth
+    lateinit var redirectLogin: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        etEmail = findViewById(R.id.regemail)
+        etPass = findViewById(R.id.regpass)
+        userName = findViewById(R.id.reguser)
+        btnSignUp = findViewById(R.id.signupbutton)
+        redirectLogin = findViewById(R.id.textViewlogin)
+
+        auth = FirebaseAuth.getInstance()
+        btnSignUp.setOnClickListener {
+            signUpUser()
+        }
+        redirectLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun signUpUser() {
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+        if (email.isBlank() || pass.isBlank()) {
+            Toast.makeText(this, "Email and password cannot be blank", Toast.LENGTH_LONG).show()
+            return
+        }
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this){
+            if (it.isSuccessful){
+                Toast.makeText(this, "Successfully Signed Up", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            else
+            {
+                Toast.makeText(this, "Signed Up failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
